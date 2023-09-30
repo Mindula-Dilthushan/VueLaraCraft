dfc73cbee8127b1e6784b76882c807983936d094
Mindula-Dilthushan <minduladilthushan1@gmail.com>
2023-09-30 10:32:50 +0530

    update-project

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,9 @@
-import './bootstrap';
+require('./bootstrap');
+
+import { createApp } from 'vue';
+
+const app = createApp({});
+
+app.component('example-component', require('./components/ExampleComponent.vue').default);
+
+app.mount('#app');
---
+++ resources/js/components/ExampleComponent.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h1>Hello from Vue.js Component!</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    // Component logic goes here
+}
+</script>
+
+<style>
+/* Component styles go here */
+</style>
